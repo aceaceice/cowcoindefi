d0fd9e58eb0972296cb761656165b59f18faa28c
unknown <notascamtrustmebro@gmail.com>
2021-12-30 20:06:45 +0200

    Updates

---
+++ precache-manifest.80b90254142b31dc0ffe47fa368e147e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56158faaa140108fe68215b9f7c8f817",
+    "url": "/cowcoindefi/index.html"
+  },
+  {
+    "revision": "da5ac25d4d8790c10de4",
+    "url": "/cowcoindefi/static/css/main.c3ecde01.chunk.css"
+  },
+  {
+    "revision": "2fcfffb84ea679ada958",
+    "url": "/cowcoindefi/static/js/2.b9ee28ea.chunk.js"
+  },
+  {
+    "revision": "da5ac25d4d8790c10de4",
+    "url": "/cowcoindefi/static/js/main.0a104f21.chunk.js"
+  },
+  {
+    "revision": "a21d8d9bfd4ced6f9914",
+    "url": "/cowcoindefi/static/js/runtime-main.9a9d295e.js"
+  }
+]);
